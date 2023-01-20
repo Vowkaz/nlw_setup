a2702ad60026acf51ba2e656fa3ea4ba641da601
vowkaz <jlucasgarmier@gmail.com>
2023-01-19 21:03:19 -0400

    prisma integrado

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,29 @@
-/*
-* Usar TypeScript:: Vowkaz 19/01/2023 created
+/**
+* Backend API REST:: Vowkaz 19/01/2023 created
 */
+/**
+* Metodos HTTP : Get, Post, Put, Delete, Patch
+*/
+import Fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
+const app = Fastify()
+const prisma = new PrismaClient()
+
+app.get( '/', async () => {
+    const habits = await prisma.habit.findMany({
+        where: {
+            title : {
+                startsWith: 'beber'
+            }
+        }
+    })
+
+    return habits
+})
+
+app.listen({
+    port: 3333
+}) .then(() => {
+    console.log('server up')
+})
